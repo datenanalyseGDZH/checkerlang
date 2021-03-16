e66d6c7d66e8cfacb073650b0d5b223b1387b7d7
Damian Brunold <dab@dabsoft.ch>
2021-03-16 20:45:59 +0100

    Rework js module loader to support system and local modules

--- ckl-js/js/moduleloader.mjs
+++ ckl-js/js/moduleloader.mjs
@@ -2,14 +2,15 @@ import { RuntimeError } from "./errors.mjs";
 import { modulemath } from "./module_math.mjs";
 
 export const moduleloader = function(modulefile, pos, fs) {
-    if (!modulefile.startsWith(".")) {
-        let module = modulefile;
-        if (module.endsWith(".ckl")) module = module.substr(0, module.length - 4);
-        switch (module) {
-            case "math": return modulemath;
-            default: throw new RuntimeError("unknown system module " + module, pos);
-        }
-    } else {
-        return fs.readFileSync(modulefile, {encoding: 'utf8', flag: 'r'});
+    switch (modulefile) {
+        case "math.ckl": return modulemath;
+        // TODO add other system modules
+        default:
+            // TODO prevent directory traversal, but allow some relative paths?!
+            // TODO configure base module path
+            const path = modulefile.replace(/\\/g, "/").split("/");
+            const filename = path[path.length - 1];
+            if (!fs.existsSync(filename)) throw new RuntimeError("unknown system module " + modulefile.substr(0, modulefile.length - 4), pos);
+            return fs.readFileSync(filename, {encoding: 'utf8', flag: 'r'});
     }
 };
