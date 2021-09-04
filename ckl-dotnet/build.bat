51816b88d813eb1e6964f9aab00631bda82d0e11
Damian Brunold <damian.brunold@bluewin.ch>
2021-09-04 20:18:47 +0200

    Switch ckl-dotnet build from batch to ckl

--- ckl-dotnet/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd ckl-dotnet
-dotnet publish -c Release --output ..\dist --self-contained --nologo
-cd ..\ckl-run
-dotnet publish --output ..\dist --self-contained --nologo --runtime win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -c Release
-cd ..\ckl-repl
-dotnet publish --output ..\dist --self-contained --nologo --runtime win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -c Release
-cd ..
-
