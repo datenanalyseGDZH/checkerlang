cbe7e77e84f85b1b5bd4abb752cbe92f98e7180c
Damian Brunold <dab@dabsoft.ch>
2022-06-23 22:49:11 +0200

    Handle def of variable class

--- ckl-js/js/parser.mjs
+++ ckl-js/js/parser.mjs
@@ -225,7 +225,7 @@ export class Parser {
             } else {
                 // handle single var def
                 let token = lexer.next();
-                if (token.type === "identifier" && token.value === "class") {
+                if (token.type === "identifier" && token.value === "class" && lexer.peek().type === "identifier") {
                     token = lexer.next();
                     if (token.type === "keyword") throw new SyntaxError("Cannot redefine keyword '" + token + "'", token.pos);
                     if (token.type !== "identifier") throw new SyntaxError("Expected identifier but got '" + token + "'", token.pos);
